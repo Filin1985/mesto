61af5068d1d7676519f56fa9d45d8e958ced5007
Marat Ikhsanov <ihsmen@gmail.com>
2023-03-02 06:05:55 +0300

    fix: Исправить замечания после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,44 @@
 // Селекторы кнопок
-const editProfileOpenButton = document.querySelector(".button_type_edit")
-const closeEditProfileButton = document.querySelector(".button_type_close")
+const editProfileOpenButton = document.querySelector(".button_type_edit");
+const closeEditProfileButton = document.querySelector(".button_type_close");
 
 // Селектор popup
-const popupProfileForm = document.querySelector(".popup")
+const popupProfileForm = document.querySelector(".popup");
 
 // Селекторы полей профиля
-let nameProfile = document.querySelector(".profile__name")
-let profProfile = document.querySelector(".profile__prof")
+let nameProfile = document.querySelector(".profile__name");
+let profProfile = document.querySelector(".profile__prof");
 
 // Селекторы полей popup профиля
-const nameProfileInput = popupProfileForm.querySelector(".popup__item_el_name")
-const profProfileInput = popupProfileForm.querySelector(".popup__item_el_prof")
+const nameProfileInput = popupProfileForm.querySelector(".popup__item_el_name");
+const profProfileInput = popupProfileForm.querySelector(".popup__item_el_prof");
 
 editProfileOpenButton.addEventListener("click", () => {
-  nameProfileInput.value = nameProfile.textContent
-  profProfileInput.value = profProfile.textContent
-  openPopup()
-})
+  nameProfileInput.value = nameProfile.textContent;
+  profProfileInput.value = profProfile.textContent;
+  openPopup();
+});
 
-closeEditProfileButton.addEventListener("click", closePopup)
+closeEditProfileButton.addEventListener("click", closePopup);
 
 popupProfileForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  setProfileValues(nameProfileInput.value, profProfileInput.value)
-  closePopup()
-})
+  e.preventDefault();
+  setProfileValues(nameProfileInput.value, profProfileInput.value);
+  closePopup();
+});
 
 // Функция открытия popup
 function openPopup() {
-  console.log(nameProfileInput.textContent)
-  popupProfileForm.classList.add("popup_opened")
+  popupProfileForm.classList.add("popup_opened");
 }
 
 // Функция закрытия popup
 function closePopup() {
-  popupProfileForm.classList.remove("popup_opened")
+  popupProfileForm.classList.remove("popup_opened");
 }
 
 // Функция редактирования профиля
 function setProfileValues(newName, newProf) {
-  nameProfile.textContent = newName
-  profProfile.textContent = newProf
+  nameProfile.textContent = newName;
+  profProfile.textContent = newProf;
 }
